b993dfef63546a186ea7a78a9bb34a6196f4e18e
Reshetov Yuriy <ryv1983@mail.ru>
2024-03-24 23:07:57 +0300

    bit manipulation last edit

--- ASSIGNMENTS/5_Challenge_BitFields/ex5_adv.c
+++ ASSIGNMENTS/5_Challenge_BitFields/ex5_adv.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 struct onscreen_box
 {
-    unsigned int : 4;
-    unsigned int opaque : 1;
-    unsigned int f_color : 3;
-    unsigned int : 2;
-    unsigned int dysplay : 1;
-    unsigned int b_color : 3;
-    unsigned int type_border : 2
+    unsigned char : 4;
+    unsigned char opaque : 1;
+    unsigned char f_color : 3;
+    unsigned char : 2;
+    unsigned char dysplay : 1;
+    unsigned char b_color : 3;
+    unsigned char type_border : 2;
 };
 
 char *types_colors[8] = {"Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White"};
@@ -21,19 +22,25 @@ char *types_dysplay[2] = {"Shown", "Hidden"};
 
 int main(void)
 {
-
-    struct onscreen_box user_settings = {0, +1, +4, 0, +1, +3, 1};
+    struct onscreen_box user_settings = {1, 4, 1, 3, 1};
     printf("Your settings are:\nBox is %s\nThe fill color is %s\nBorder is %s\nThe border color is %s\nThe border style is%s\n",
            types_opacity[user_settings.opaque], types_colors[user_settings.f_color], types_dysplay[user_settings.dysplay], types_colors[user_settings.b_color], types_borders[user_settings.type_border]);
 
     user_settings.opaque = 0;
     user_settings.f_color = 7;
+    // user_settings["dysplay"] = 1; // To implement hash-table needs to visit site https://benhoyt.com/writings/hash-table-in-c/
     user_settings.dysplay = 1;
     user_settings.b_color = 7;
     user_settings.type_border = 2;
 
     printf("\n\nModified settings are:\nBox is %s\nThe fill color is %s\nBorder is %s\nThe border color is %s\nThe border style is%s\n",
            types_opacity[user_settings.opaque], types_colors[user_settings.f_color], types_dysplay[user_settings.dysplay], types_colors[user_settings.b_color], types_borders[user_settings.type_border]);
+
+    unsigned char num = 0b10000000;
+    printf("The size of boolean is %d\n", num);
+    printf("The size of boolean is %0x\n", num);
+
+    printf("The size of boolean is %0lx\n", sizeof(struct onscreen_box));
 
     return 0;
 }
